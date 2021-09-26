4d6bcf081ab697a8b55f866dbf2dab3ec8373745
sugarnyang <72382334+sugarnyang@users.noreply.github.com>
2021-09-26 14:31:08 +0900

    Queue를 ArrayList로 구현

---
+++ src/MyQueue.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class MyQueue<T> {
+    private ArrayList<T> queue = new ArrayList<T>();
+
+    public ArrayList enqueue(T item) {
+        queue.add(item);
+        return queue;
+    }
+
+    public ArrayList dequeue() {
+        if(queue.isEmpty()){
+            return null;
+        }
+        queue.remove(0);
+        return queue;
+    }
+
+    public static void main(String[] args) {
+        MyQueue<Integer> mq = new MyQueue<Integer>();
+        System.out.println(mq.enqueue(1));
+        System.out.println(mq.enqueue(2));
+        System.out.println(mq.enqueue(3));
+        System.out.println(mq.dequeue());
+        System.out.println(mq.dequeue());
+        System.out.println(mq.dequeue());
+        System.out.println(mq.dequeue());
+        System.out.println(mq.dequeue());
+    }
+}
